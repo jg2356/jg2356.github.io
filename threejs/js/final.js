b85059e1e20f6d8ac1e2376cac891525dc9fa759
Jose Gomez <k13gomez@gmail.com>
2015-04-03 23:33:59 -0400

    added final project files

---
+++ threejs/js/final.js
@@ -0,0 +1,113 @@
+/***********
+ * final.js
+ * Final Project
+ * J. Gomez
+ * April 4, 2014
+ ***********/
+
+var camera, scene, renderer;
+var cameraControls;
+var clock = new THREE.Clock();
+
+function createStairs(riser, tread, width, nbrSteps) {
+  var geom = new THREE.Geometry();
+  // create the initial two vertices, the base of the stairs
+  geom.vertices.push(new THREE.Vector3(0, 0, 0));
+  geom.vertices.push(new THREE.Vector3(0, 0, width));
+
+  //steps
+  for (var i = 0; i < nbrSteps; i++) {
+    // get the last two vertices added, the base of the current step
+    var ol = geom.vertices[geom.vertices.length - 2];
+    var or = geom.vertices[geom.vertices.length - 1];
+
+    // create four vertices, rising and then treading on each side (l, r)
+    var v1 = new THREE.Vector3(ol.x, ol.y + riser, ol.z);
+    var v2 = new THREE.Vector3(or.x, or.y + riser, or.z);
+    var v3 = new THREE.Vector3(ol.x + tread, ol.y + riser, ol.z);
+    var v4 = new THREE.Vector3(or.x + tread, or.y + riser, or.z);
+    geom.vertices.push(v1, v2, v3, v4);
+
+    // create the faces using the last 6 vertices added to our geometry
+    var vi = geom.vertices.length;
+    geom.faces.push(new THREE.Face3(vi - 6, vi - 5, vi - 4));
+    geom.faces.push(new THREE.Face3(vi - 5, vi - 4, vi - 3));
+    geom.faces.push(new THREE.Face3(vi - 4, vi - 3, vi - 2));
+    geom.faces.push(new THREE.Face3(vi - 3, vi - 2, vi - 1));
+  }
+
+  geom.computeFaceNormals();
+
+  return geom;
+}
+
+function createScene() {
+  var geom = createStairs(1, 2, 4, 5);
+
+  // red lambert material, which is affected by lights
+  var mat = new THREE.MeshLambertMaterial({color: 0xFF0000, shading: THREE.FlatShading, side: THREE.DoubleSide});
+
+  // create our mesh
+  var mesh = new THREE.Mesh(geom, mat);
+
+  // lights
+  var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+  light.position.set(-15, 8, 2);
+  var ambientLight = new THREE.AmbientLight(0x222222);
+  scene.add(light);
+  scene.add(ambientLight);
+
+  // add to the scene
+  scene.add(mesh);
+}
+
+function init() {
+  var canvasWidth = window.innerWidth;
+  var canvasHeight = window.innerHeight;
+  var canvasRatio = canvasWidth / canvasHeight;
+
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer({antialias : true, preserveDrawingBuffer: true});
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
+  renderer.setSize(canvasWidth, canvasHeight);
+  // set the clear color to black, for our open box scene
+  renderer.setClearColor(0x000000, 1.0);
+
+  // set the camera position for looking at our open box
+  // and point the camera at our target
+  var target = new THREE.Vector3(0, 0, 0);
+  camera = new THREE.PerspectiveCamera(40, canvasRatio, 1, 1000);
+  camera.position.set(-15, 20, 20);
+  camera.lookAt(target);
+  cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+  cameraControls.target = target;
+  cameraControls.center = target;
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  var delta = clock.getDelta();
+  cameraControls.update(delta);
+  renderer.render(scene, camera);
+}
+
+function addToDOM() {
+  var container = document.getElementById('container');
+  var canvas = container.getElementsByTagName('canvas');
+  if (canvas.length>0) {
+    container.removeChild(canvas[0]);
+  }
+  container.appendChild( renderer.domElement );
+}
+
+init();
+createScene();
+addToDOM();
+render();
+animate();
