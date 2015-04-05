5e052471bbebd11cbc6f9c4b2e2a2fb2a084a6b7
Jose Gomez <k13gomez@gmail.com>
2015-04-04 20:07:01 -0400

    added comments

--- threejs/js/final.js
+++ threejs/js/final.js
@@ -8,9 +8,14 @@
 var camera, scene, renderer, controls, cameraControls;
 var clock = new THREE.Clock();
 
+/*
+ * Create a 3D bond with atom data (adata1, adata2) and bond data (bdata)
+ */
 function getBond(adata1, adata2, bdata) {
+  //two half bonds, one for each atom
   var bonds = new THREE.Object3D();
 
+  //create points and bond line
   var point1 = new THREE.Vector3(adata1.x, adata1.y, adata1.z);
   var point2 = new THREE.Vector3(adata2.x, adata2.y, adata2.z);
   var path = new THREE.Line3(point1, point2);
@@ -18,10 +23,12 @@ function getBond(adata1, adata2, bdata) {
   var points = [point1, point2];
   var atoms = [adata1, adata2];
 
+  //create two cylinders, one corresponding to each atom
   for(var i = 0; i < 2; i++) {
     var adata = atoms[i];
     var pointX = points[i];
     var pointY = path.center();
+    //the bond cylinder is created with the atom color
     var material = new THREE.MeshLambertMaterial({color: new THREE.Color(adata.atom.color)});
     var direction = new THREE.Vector3().subVectors(pointY, pointX);
     var orientation = new THREE.Matrix4();
@@ -30,6 +37,7 @@ function getBond(adata1, adata2, bdata) {
                                                  0, 0, 1, 0,
                                                  0, -1, 0, 0,
                                                  0, 0, 0, 1));
+    //the bond radius is n times the default radius, triple bonds are three times as wide as single bonds
     var radius = 0.20;
     var bradius = radius*bdata.bond.type;
     var geom = new THREE.CylinderGeometry(bradius, bradius, direction.length(), 32, 32);
@@ -37,6 +45,7 @@ function getBond(adata1, adata2, bdata) {
     bond.applyMatrix(orientation);
     var line = new THREE.Line3(pointX, pointY);
     bond.position = line.center();
+    //add our half bond to the bonds object
     bonds.add(bond);
   }
 
@@ -44,21 +53,30 @@ function getBond(adata1, adata2, bdata) {
   return bonds;
 }
 
+/*
+ * Create a 3D atom with atom data (adata) and opacity
+ */
 function getAtom(adata, opacity) {
+  //create the atom with the appropriate color and opacity
   var mat = new THREE.MeshLambertMaterial({color: new THREE.Color(adata.atom.color), transparent: true, opacity: opacity});
   var geom = new THREE.SphereGeometry(1, 32, 32);
   var atom = new THREE.Mesh(geom, mat);
+  //place our atom in the x, y, z plane
   atom.position.set(adata.x, adata.y, adata.z);
 
   atom.data = adata.atom;
   return atom;
 }
 
+/*
+ * Create a 3D molecular structure with molecule data (moldata) and options
+ */
 function getMolStructure(moldata, options) {
   var structure = new THREE.Object3D();
   structure.atoms = [];
   structure.bonds = [];
 
+  //parse each atom
   for(var i in moldata.atoms) {
     var adata = moldata.atoms[i];
     var atom = getAtom(adata, options.opacity);
@@ -66,6 +84,7 @@ function getMolStructure(moldata, options) {
     structure.atoms.push(atom);
   }
 
+  //parse each bond
   for(var i in moldata.bonds) {
     var bdata = moldata.bonds[i];
     var adata1 = moldata.atoms[bdata.a - 1];
@@ -86,10 +105,12 @@ function Controls(scene) {
 
   this.opacity = 0.95;
 
+  //trigger loading a file
   this.load = function() {
     molfile.click();
   };
 
+  //handle uploading a molfile and rendering the returned structure JSON
   this.upload = function() {
     if (molfile.value) {
       $.ajax({
@@ -115,12 +136,14 @@ function Controls(scene) {
     }
   };
 
+  //create our GUI, which has colors added dynamically
   this.init = function() {
     if (gui) {
       gui.destroy();
     }
 
     gui = new dat.GUI();
+    //basic controls
     gui.add($this, 'load').name('load mol file...');
     gui.add($this, 'opacity', 0.00, 1.00).name('atom opacity').step(0.05).onChange(function(value) {
       if (structure) {
@@ -131,6 +154,7 @@ function Controls(scene) {
       }
     });
 
+    //add our atom colors
     if (structure) {
       var folder = gui.addFolder('atom colors');
       folder.open();
@@ -163,6 +187,7 @@ function Controls(scene) {
 }
 
 function createScene() {
+  // only render the controls initially, the structure will be loaded when a file is uploaded
   controls = new Controls(scene).init();
 
   // lots of lights to illuminate the molecule well
